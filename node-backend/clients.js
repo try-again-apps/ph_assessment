3b5080ded2ec30be292a74c03c1c1b4ace0001a0
Try Again Apps <try.again.apps@gmail.com>
2018-09-12 23:59:42 +0200

    Add add/remove/all clients backend routes

--- node-backend/clients.js
+++ node-backend/clients.js
@@ -9,22 +9,34 @@ router.use((req, res, next) => {
   next();
 });
 
-router.get('/clients', (req, res) => {
-  res.json({});
+router.get('/all', (req, res) => {
+  Client.find((err, docs) => {
+    err ? console.info(err) : res.json(docs);
+  });
 });
 
 router.route('/add').post((req, res) => {
-  console.info(req.body);
   const item = new Client(req.body);
   console.info(item);
   item
     .save()
     .then(item => {
-      res.json('Added');
+      res.json(item);
     })
     .catch(err => {
       res.status(400).send('unable to save to database');
     });
+});
+
+router.route('/delete').delete((req, res) => {
+  console.info(req.body, 'DEL');
+  Client.findByIdAndRemove({ _id: req.body.id }, (err, item) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json('Deleted');
+    }
+  });
 });
 
 module.exports = router;
