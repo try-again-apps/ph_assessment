2f4f3ce293fb110f593ae1faa31c5569a8f5b62d
Try Again Apps <try.again.apps@gmail.com>
2018-09-13 00:04:14 +0200

    Update about page with needed features

--- react-frontend/src/components/About.jsx
+++ react-frontend/src/components/About.jsx
@@ -2,7 +2,22 @@ import React from 'react';
 
 class About extends React.PureComponent {
   render() {
-    return <div>test</div>;
+    return (
+      <div>
+        <li>Update readme files.</li>
+        <li>Remeber to cleanup package json deps for dev and build.</li>
+        <li>Optimistic update</li>
+        <li>404 page and react router *</li>
+        <li>Populate DB with fake data - maybe use faker?</li>
+        <li>Limit operations buttons width and give some space between</li>
+        <li>Clients cannot have same name - unique?</li>
+        <li>CRUD for client</li>
+        <li>CRUD for comments</li>
+        <li>getting clients from backend wiht filter, sorting and limit</li>
+        <li>Getting single clinet and comment</li>
+        <li>Use moment for date formattings</li>
+      </div>
+    );
   }
 }
 
