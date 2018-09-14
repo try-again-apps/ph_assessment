0b3c1b7ed94932f7ffbd98668ca9f6b678e88882
Try Again Apps <try.again.apps@gmail.com>
2018-09-14 23:18:45 +0200

    Update readme file and about page

--- react-frontend/src/components/About.jsx
+++ react-frontend/src/components/About.jsx
@@ -1,31 +1,50 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
-class About extends React.PureComponent {
-  render() {
-    return (
-      <div>
-        <li>Update readme files.</li>
-        <li>Remeber to cleanup package json deps for dev and build.</li>
-        <li>Optimistic update</li>
-        <li>404 page and react router *</li>
-        <li>Populate DB with fake data - maybe use faker?</li>
-        <li>Limit operations buttons width and give some space between</li>
-        <li>Clients cannot have same name - unique?</li>
-        <li>CRUD for client</li>
-        <li>CRUD for notes</li>
-        <li>getting clients from backend with filter, sorting and limit</li>
-        <li>Getting single note</li>
-        <li>(M) adding notes</li>
-        <li>(M) removing notes</li>
-        <li>(M) updating notes</li>
-        <li>(M) filtering (by status)</li>
-        <li>(M) sorting by name/status</li>
-        <li>Add busy state for every operation (Redux is ready for it)</li>
-        <li>SSR for backend</li>
-        <li>Add remove on client details page</li>
-      </div>
-    );
-  }
-}
+const About = () => (
+  <Fragment>
+    <div>
+      <p>Unfortunately due to lack of time not all was done as I wanted.</p>
+      <p>In few places redux could be used instead of internal state.</p>
+      <p>
+        Clients inline adding is not maybe very pretty but wanted to show
+        reusability of component.
+      </p>
+      <p>
+        This was my first try to react-table and I found that it does not
+        support Immutables after I did part of implementation. That is why,
+        there is ugly conversion to JS. But at least it shows that I can use
+        reduce ;)
+      </p>
+    </div>
+    <br />
+    <div>
+      Still to be done (few topics which may explain some solutions in code):
+      <li>cleanup package json deps for dev and build</li>
+      <li>optimistic updates</li>
+      <li>404 page and react router *</li>
+      <li>Populate DB with fake data - maybe use faker?</li>
+      <li>Clients cannot have same name - unique?</li>
+      <li>
+        Add busy state for every operation (Redux is prepared ready for it)
+      </li>
+      <li>SSR for backend</li>
+      <li>Add remove on client details page</li>
+      <li>Code refactoring and cleanup</li>
+      <li>production build</li>
+      <li>
+        {
+          'Statuses could be separate table and connect to client with one-to-one relationship'
+        }
+      </li>
+      <li>reorganize project structure</li>
+      <li>eject project from react-app and configure webpack</li>
+      <li>
+        {
+          "...and most probably many more but I'm too tired this week to think of some more ;)"
+        }
+      </li>
+    </div>
+  </Fragment>
+);
 
 export default About;
